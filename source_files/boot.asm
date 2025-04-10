d8197edca20ab1a972e098408f104374cc7d95f9
susikohmelo <lassi.jylhankangas@gmail.com>
2025-04-10 22:50:58 +0300

    simplified bootloading message printing and saved memory from sector 1

--- source_files/boot.asm
+++ source_files/boot.asm
@@ -1,55 +1,64 @@
-; A memory offset of is needed, mostly for the strings
+; A memory offset is needed
 [org 0x7c00]
 
+mov		[disk_num], dl ; Save the disk number
 
-mov [disk_num], dl ; Save the disk number
-
-mov ah, 0x0e
-mov bx, boot_msg
-print_boot_msg:
-	mov al, [bx]
-	int 0x10
-	inc bx
-	cmp al, 0
-	jne print_boot_msg
+; Boot message
+mov		bx, boot_msg
+mov		[message_ptr], bx
+call	print_message
 
 ; Wait for input
-mov ah, 0
-int 0x16
+mov		ah, 0
+int		0x16
 
-mov es, [number_zero] ; Extra segment - we won't need it
-mov ah, 2 ; BIOS read sectors mode
-mov al, 1 ; N sectors to read
-mov ch, 0 ; Cylinder num
-mov dh, 0 ; Head num
-mov cl, 2 ; Sector num
-mov dl, [disk_num]
-mov bx, 0x7e00 ; Location to read memory into
-int 0x13 ; Read disk
+; Reading sector 2 from the disk
+mov		es, [number_zero] ; Extra segment - we won't need it
+mov		ah, 2 ; BIOS read sectors mode
+mov		al, 1 ; N sectors to read
+mov		ch, 0 ; Cylinder num
+mov		dh, 0 ; Head num
+mov		cl, 2 ; Sector num
+mov		dl, [disk_num]
+mov		bx, 0x7e00 ; Location to read memory into
+int		0x13 ; Read disk
 
 ; Read error checks
-jc  read_error ; Jump if carry
-cmp al, 1 ; Num of sectors read - should be 1
-jne read_error
+jc		read_error ; Jump if carry
+cmp		al, 1 ; Num of sectors read - should be 1
+je		read_success
 
-; Read succesful!
-mov ah, 0x0e
-mov bx, read_success_msg
-print_success:
-	mov al, [bx]
-	int 0x10
-	inc bx
-	cmp al, 0
-	jne print_success
+read_error:
+	mov		bx, read_error_msg
+	mov		[message_ptr], bx
+	call	print_message
+	jmp		$
+
+read_success:
+	; Read succesful message
+	mov		bx, read_success_msg
+	mov		[message_ptr], bx
+	call	print_message
 
 ; Wait for input
-mov ah, 0
-int 0x16
+mov		ah, 0
+int		0x16
 
 jmp SECTOR2_START
 
-read_error:
-	db "Error reading disk :<", 0
+print_message:
+	mov		ah, 0x0e
+	mov		bx, [message_ptr]
+	print_loop:
+		mov		al, [bx]
+		int		0x10
+		inc		bx
+		cmp		al, 0
+		jne		print_loop
+		ret		
+
+message_ptr: ; Temp memory to store print parameter in
+	times 2 db 0
 
 boot_msg:
 	db 0xa, 0xd ; \n \r
@@ -59,6 +68,17 @@ boot_msg:
 	db 0xa, 0xd ; \n \r
 	db "Waiting for input ..."
 	db 0xa, 0xd, 0 ; \n \r NULL
+
+read_error_msg:
+	db "Error reading disk :<", 0
+
+read_success_msg:
+	db 0xa, 0xd ; \n \r
+	db "Success!"
+	db 0xa, 0xd ; \n \r
+	db "Enter protected mode & boot into DOROS?"
+	db 0xa, 0xd ; \n \r
+	db "Waiting for input ...", 0
 
 disk_num: ; Variable for storing the disk the boot sector is located in
 	db 0
@@ -72,13 +92,6 @@ times 510 - ($ - $$) db 0
 db 0x55, 0xaa
 
 ; SECTOR 2 STARTS HERE --------------------------------------------------------
-read_success_msg:
-	db 0xa, 0xd ; \n \r
-	db "Success!"
-	db 0xa, 0xd ; \n \r
-	db "Enter protected mode & boot into DOROS?"
-	db 0xa, 0xd ; \n \r
-	db "Waiting for input ...", 0
 
 SECTOR2_START: ; Used as a label to jump past the previously stored messages
 jmp $
