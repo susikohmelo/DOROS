b6025c46f53b30a26584579c0729c65c8728a402
Lassi Jylhankangas <ljylhank@c1r5p19.hive.fi>
2025-04-11 19:38:45 +0300

    removed typo

--- source_files/boot/enter_protected_mode.asm
+++ source_files/boot/enter_protected_mode.asm
@@ -7,7 +7,6 @@ setup_protected_mode:
 	mov		eax, cr0 
 	or		eax, 0x1
 	mov		cr0, eax ; Now we are in 32bit mode
-A
 	jmp		CODE_SEG:enter_protected_mode ; Far jump to code segment
 
 %include"source_files/boot/GDT.asm"
