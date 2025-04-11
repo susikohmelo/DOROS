aa8d61510aecf49ad6cb3e6285eb399fbd058ba0
Lassi Jylhankangas <ljylhank@c1r5p19.hive.fi>
2025-04-11 19:36:42 +0300

    changed .asm file formatting to fit the github 8 width tabs

--- source_files/boot/GDT.asm
+++ source_files/boot/GDT.asm
@@ -8,50 +8,49 @@ CODE_SEG equ code_descriptor - GDT_start
 DATA_SEG equ data_descriptor - GDT_start
 
 GDT_start:
-	empty_descriptor: ; 	-	8 Byte segment
-		dd	0x0 ; Four 0 bytes
-		dd	0x0 ; Four 0 bytes
-	
-	code_descriptor:
-		dw	0xFFFF ; Limit	-	16	bits (+ 4 later)
-		dw	0x0 ; Base 		-	16	bits
-		db	0x0 ; Base 		-	8	bits (+ 8 later)
+empty_descriptor: ; 	-	8 Byte segment
+	dd	0x0 ; Four 0 bytes
+	dd	0x0 ; Four 0 bytes
 
-		; Present=1		| 1 bit	|	Is segment used?
-		; Privilege=0	| 2 bit	|	Segment hierarchy/privs, 0 is highest
-		; Type=1		| 1 bit |	Set to 1 if segment is code or data
-		; Type flags ------------------
-		; Code=1		| 1 bit |	Has code?
-		; Conforming=0	| 1 bit |	Can be executed from a lower privelege?
-		; Readable=1	| 1 bit |	Able to read constants?
-		; Accessed=0	| 1 bit |	This flag is used by the CPU. Always set 0!
-		; Other flags -----------------
-		; Granularity=1	| 1 bit |	Multiplies limit by 0x1000, ie more space.
-		; 32bit=1		| 1 bit |	Use 32 bit memory?
-		db	0b10011010	; Pres|Priv|Type|Typeflags
-		db	0b11001111	; OtherFlags|Last 4 bits of limit
-		db	0x0			; Last 8 bits of base
+code_descriptor:
+	dw	0xFFFF ; Limit	-	16 bits (+ 4 later)
+	dw	0x0 ; Base 	-	16 bits
+	db	0x0 ; Base	-	8  bits (+ 8 later)
 
-	data_descriptor:
-		dw	0xFFFF ; Limit	-	16	bits (+ 4 later)
-		dw	0x0 ; Base 		-	16	bits
-		db	0x0 ; Base 		-	8	bits (+ 8 later)
+	; Present=1	| 1 bit	|	Is segment used?
+	; Privilege=0	| 2 bit	|	Segment hierarchy/privs, 0 is highest
+	; Type=1	| 1 bit |	Set to 1 if segment is code or data
+	; Type flags ------------------
+	; Code=1	| 1 bit |	Has code?
+	; Conforming=0	| 1 bit |	Can be executed from a lower privelege?
+	; Readable=1	| 1 bit |	Able to read constants?
+	; Accessed=0	| 1 bit |	This flag is used by the CPU. Always 0!
+	; Other flags -----------------
+	; Granularity=1	| 1 bit |	Multiplies limit by 4096, ie more space
+	; 32bit=1	| 1 bit |	Use 32 bit memory?
+	db	0b10011010		; Pres|Priv|Type|Typeflags
+	db	0b11001111		; OtherFlags|Last 4 bits of limit
+	db	0x0			; Last 8 bits of base
 
-		; Present=1		| 1 bit	|	Is segment used?
-		; Privilege=0	| 2 bit	|	Segment hierarchy/privs, 0 is highest
-		; Type=1		| 1 bit |	Set to 1 if segment is code or data
-		; Type flags ------------------
-		; Code=0		| 1 bit |	Has code?
-		; Direction=0	| 1 bit |	Should segment grow down instead of up?
-		; Writable=1	| 1 bit |	Able to write to this segment?
-		; Accessed=0	| 1 bit |	This flag is used by the CPU. Always set 0
-		; Other flags -----------------
-		; Granularity=1	| 1 bit |	Multiplies limit by 0x1000, ie more space.
-		; 32bit=1		| 1 bit |	Use 32 bit memory?
-		db	0b10010010	; Pres|Priv|Type|Typeflags
-		db	0b11001111	; OtherFlags|Last 4 bits of limit
-		db	0x0			; Last 8 bits of base
+data_descriptor:
+	dw	0xFFFF ; Limit	-	16	bits (+ 4 later)
+	dw	0x0 ; Base 		-	16	bits
+	db	0x0 ; Base 		-	8	bits (+ 8 later)
 
+	; Present=1	| 1 bit	|	Is segment used?
+	; Privilege=0	| 2 bit	|	Segment hierarchy/privs, 0 is highest
+	; Type=1	| 1 bit |	Set to 1 if segment is code or data
+	; Type flags ------------------
+	; Code=0	| 1 bit |	Has code?
+	; Direction=0	| 1 bit |	Should segment grow down instead of up?
+	; Writable=1	| 1 bit |	Able to write to this segment?
+	; Accessed=0	| 1 bit |	This flag is used by the CPU. Always 0!
+	; Other flags -----------------
+	; Granularity=1	| 1 bit |	Multiplies limit by 4096, ie more space.
+	; 32bit=1	| 1 bit |	Use 32 bit memory?
+	db	0b10010010	;	Pres|Priv|Type|Typeflags
+	db	0b11001111	; 	OtherFlags|Last 4 bits of limit
+	db	0x0			; Last 8 bits of base
 GDT_end:
 
 GDT_descriptor:
--- source_files/boot/enter_protected_mode.asm
+++ source_files/boot/enter_protected_mode.asm
@@ -1,13 +1,13 @@
 setup_protected_mode:
 
-	cli		; Clear interrupts
+	cli	; Clear interrupts
 	lgdt	[GDT_descriptor] ; Load GDT
 
 	; Change the last bit of the cr0 register to 1
 	mov		eax, cr0 
 	or		eax, 0x1
 	mov		cr0, eax ; Now we are in 32bit mode
-
+A
 	jmp		CODE_SEG:enter_protected_mode ; Far jump to code segment
 
 %include"source_files/boot/GDT.asm"
