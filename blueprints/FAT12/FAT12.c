fae89d22a63935a61f47e13fda126532dbbf1277
susikohmelo <lassi.jylhankangas@gmail.com>
2025-04-13 15:57:18 +0300

    very very minor WIP update

--- blueprints/FAT12/FAT12.c
+++ blueprints/FAT12/FAT12.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+typedef struct s_bootsector
+{
+}	t_bootsector
 
 int	main(int argc, char **argv)
 {
 
 	if (argc < 3)
-	
+	{
+		printf("%s\n", "Bad argc");
+		return 1;
+	}
+
+	// Read in binary mode
+	FILE *disk = fopen(argc[1], "rb");
+
+	return 0;
 }
