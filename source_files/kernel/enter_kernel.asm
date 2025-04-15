bfbf809ef21950c3d0a4466dc9d7541894c93bbc
susikohmelo <lassi.jylhankangas@gmail.com>
2025-04-15 19:02:25 +0300

    Removed kernel.asm and added minor documentation to enter_kernel

--- source_files/kernel/enter_kernel.asm
+++ source_files/kernel/enter_kernel.asm
@@ -1,3 +1,9 @@
+; This is just a very simple way to link the previous asm bootloader to the
+; C based kernel.
+;
+; TODO
+; This is actually largely unnecessary and will likely be removed later
+
 section .text
 	[bits 32]
 	[extern main]
